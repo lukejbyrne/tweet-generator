d8baf2c25ed12874ce32a590ae6fc54c8bd8103d
lukejbyrne <144802491+lukejbyrne@users.noreply.github.com>
2025-01-22 13:26:07 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 from huggingface_hub import InferenceClient
+import os
 
 app = Flask(__name__)
 client = InferenceClient(api_key="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")
